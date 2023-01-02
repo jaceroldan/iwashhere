fe96e4c6d2e06c5395b8d51fd143cb510d041812
Jace Roldan <jaceroldan16@gmail.com>
2023-01-02 18:27:38 +0800

    Initial commit

---
+++ inventory/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+from inventory.models import (
+    Customer,
+    ProductType,
+    Product,
+    Transaction,
+    LineItem,
+    PaymentOption
+)
+
+@admin.register(PaymentOption)
+class PaymentOptionAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'contact_number',)
+
+
+@admin.register(ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        'customer', 'date_created', 'date_paid', 'weight',
+        'date_claimed', 'date_required', 'remarks', 'payment_status')
+
+
+@admin.register(LineItem)
+class LineItemAdmin(admin.ModelAdmin):
+    list_display = ('transaction', 'product', 'quantity', 'price_override')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'product_type', 'unit_price', 'date_created')
---
+++ inventory/models.py
@@ -0,0 +1,87 @@
+from django.db import models
+from django.db.models import Sum
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    contact_number = models.CharField(max_length=11)
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
+
+class ProductType(models.Model):
+    name = models.CharField(max_length = 256)
+
+    def __str__(self):
+        return f'Product Type: {self.name}'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    quantity = models.IntegerField(null=True, blank=True)
+    product_type = models.ForeignKey(
+        ProductType, related_name='products', on_delete=models.CASCADE)
+    unit_price = models.DecimalField(max_digits=7, decimal_places=2)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Product: {self.name} @ PHP {self.unit_price} ({self.quantity} remaining)'
+
+
+class PaymentOption(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Invoice(models.Model):
+    is_paid = models.BooleanField(default=False)
+    
+
+class Transaction(models.Model):
+    NOT_PAID = 0
+    DOWNPAYMENT_RECEIVED = 1
+    FULLY_PAID = 2
+    PAYMENT_STATUSES = (
+        (NOT_PAID, 'Not paid'),
+        (DOWNPAYMENT_RECEIVED, 'Downpayment received'),
+        (FULLY_PAID, 'Fully paid'),
+    )
+
+    customer = models.ForeignKey(
+        Customer, related_name='transactions', on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_paid = models.DateTimeField()
+    weight = models.DecimalField(decimal_places=2, max_digits=5)
+    date_claimed = models.DateTimeField()
+    date_required = models.DateTimeField()
+    payment_option = models.OneToOneField(
+        PaymentOption, related_name='payment_option', on_delete=models.CASCADE)
+    payment_status = models.IntegerField(choices=PAYMENT_STATUSES, default=NOT_PAID)
+    remarks = models.TextField()
+
+    def __str__(self):
+        return f'Transaction by {self.customer} on {self.date_created}'
+
+    @property
+    def total_price(self):
+        price = 0.0
+        for item in self.line_items.iterate():
+            price += item.total_price
+        return price
+
+
+class LineItem(models.Model):
+    transaction = models.ForeignKey(
+        Transaction, related_name='line_items', on_delete=models.CASCADE)
+    product = models.ForeignKey(
+        Product, related_name='line_items', on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    price_override = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+
+    @property
+    def total_price(self):
+        if self.price_override:
+            return self.price_override
+        return self.quantity * self.product.unit_price
