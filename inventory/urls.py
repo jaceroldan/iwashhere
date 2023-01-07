f647f5288d5959fc4b7a59d0268a37d481b013ba
Jace Roldan <jaceroldan16@gmail.com>
2023-01-07 10:51:55 +0800

    [#1] Add navigation system for viewing orders

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'inventory'
 urlpatterns = [
-    path('receipt/new/', views.create_receipt, name='new'),
+    path('order/new/', views.show_create_receipt, name='new'),
+    path('order/create/', views.create_receipt, name='create'),
+    path('order/view/<int:order_id>', views.show_order, name='view'),
+    path('list/', views.list_customers, name='list-orders'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,63 @@
-from django.shortcuts import render
+from datetime import datetime
+
+from django.shortcuts import render, redirect
+
+from inventory.models import Customer, Order
 
 # Create your views here.
-def create_receipt(request):
+def show_create_receipt(request):
     return render(request, 'inventory/receipt.html')
+
+def show_order(request, order_id):
+    order = Order.objects.get(pk=order_id)
+    context = {'order': order}
+    return render(request, 'inventory/view_order.html', context)
+
+
+def create_receipt(request):
+    customer = request.POST['customer']
+    contact_number = request.POST['contact_number']
+    customer = Customer.objects.create(
+        first_name=customer,
+        contact_number=contact_number
+    )
+
+    weight = request.POST['weight']
+    # TODO: What do I do with this?
+    # service_type = request.POST['service_type']
+    wash_cost = request.POST['wash_cost']
+    dry_cost = request.POST['dry_cost']
+    # TODO: Ask about the fold cost
+    fold_cost = request.POST['fold_cost']
+    detergent_cost = request.POST['detergent_cost']
+    fabcon_cost = request.POST['fabcon_cost']
+    bleach_cost = request.POST['bleach_cost']
+    bleach_cost = request.POST['bleach_cost']
+    plastic_cost = request.POST['plastic_cost']
+    date_required = request.POST['date_required']
+    time_required = request.POST['time_required']
+
+    date_required = datetime.strptime(f'{date_required} {time_required}', '%Y-%m-%d %H:%M')
+
+    Order.objects.create(
+        customer=customer,
+        weight=weight,
+        wash_cost=wash_cost,
+        dry_cost=dry_cost,
+        # fold_cost=fold_cost,
+        detergent_cost=detergent_cost,
+        fabcon_cost=fabcon_cost,
+        bleach_cost=bleach_cost,
+        plastic_cost=plastic_cost,
+        date_required=date_required
+    )
+
+    return redirect('inventory:list')
+
+
+def list_customers(request):
+    orders = Order.objects.all()
+    context = {
+        'orders': orders
+    }
+    return render(request, 'inventory/masterlist.html', context)
